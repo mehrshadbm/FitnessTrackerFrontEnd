8ac3dfe8618df1bfd023e10ec0b61d2ae763c172
Mehrshad Bahmani <mehrshadbahmani79@gmail.com>
2025-01-09 18:10:16 +1100

    baisc improvements

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,scss}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+    }
+  },
+  plugins: []
+};
